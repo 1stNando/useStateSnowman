bc8ff2533d6e7d61fc91afb9fb883621788bf8e0
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-07-25 16:15:30 -0400

    Basic layout of alphabet and images folder created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
 import React from 'react'
 
+//
+
 export function App() {
-  return <div>Hello, World </div>
+  // Space for code
+
+  return (
+    <main>
+      <h1>Title Head</h1>
+      <ul className="alphabet-list">
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+        <li>D</li>
+        <li>E</li>
+        <li>F</li>
+        <li>G</li>
+        <li>H</li>
+        <li>I</li>
+        <li>J</li>
+        <li>K</li>
+        <li>L</li>
+        <li>M</li>
+        <li>N</li>
+        <li>O</li>
+        <li>P</li>
+        <li>Q</li>
+        <li>R</li>
+        <li>S</li>
+        <li>T</li>
+        <li>U</li>
+        <li>V</li>
+        <li>W</li>
+        <li>X</li>
+        <li>Y</li>
+        <li>Z</li>
+      </ul>
+    </main>
+  )
 }
