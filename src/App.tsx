f7ccc3b88068924a8e0b53d4f0e64faad3b0c3e2
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-07-25 16:46:00 -0400

    Styling basic unknown word box.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-
-//
+import step_7 from '/src/images/step_7.png'
 
 export function App() {
   // Space for code
@@ -8,6 +7,18 @@ export function App() {
   return (
     <main>
       <h1>Title Head</h1>
+      <span>SpaceForAnImage</span>
+      <section>
+        <ul className="unknown-word-box">
+          <li>_</li>
+          <li>_</li>
+          <li>_</li>
+          <li>_</li>
+          <li>_</li>
+          <li>_</li>
+          <li>_</li>
+        </ul>
+      </section>
       <ul className="alphabet-list">
         <li>A</li>
         <li>B</li>
