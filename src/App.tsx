d86c3ca883695ee7536ff486c5c2b79803901e9a
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-08-01 17:53:55 -0400

    Aesthetics and final touches for a complete basic layout and working game completed.

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,7 @@ export function App() {
         </h2>
 
         <div>
-          <div>WordGuess</div>
+          <div>Guess the secret word to reveal the Snowman</div>
         </div>
 
         <div>
@@ -101,55 +101,22 @@ export function App() {
         </div>
 
         <div>
+          {
+            'Refresh the page to reset the game. If you completed the SnowMan click this button to play again '
+          }
           <button
             onClick={() => handleNewGame()}
             disabled={snowmanImageFetch !== 7}
           >
-            Click me to play new game.
+            new game.
           </button>
         </div>
+        <footer>
+          {' '}
+          Build by Fernando with the help of <br></br> Suncoast Developers Guild
+          in 2023{' '}
+        </footer>
       </main>
     </div>
   )
 }
-
-/* <img className="man" src={step_7}></img>
-      <section>
-        <ul className="unknown-word-box">
-          <li>_</li>
-          <li>_</li>
-          <li>_</li>
-          <li>_</li>
-          <li>_</li>
-          <li>_</li>
-          <li>_</li>
-        </ul>
-      </section>
-      <ul className="alphabet-list">
-        <li onClick={handleClickLetter}>A</li>
-        <li onClick={handleClickLetter}>B</li>
-        <li onClick={handleClickLetter}>C</li>
-        <li onClick={handleClickLetter}>D</li>
-        <li onClick={handleClickLetter}>E</li>
-        <li onClick={handleClickLetter}>F</li>
-        <li onClick={handleClickLetter}>G</li>
-        <li onClick={handleClickLetter}>H</li>
-        <li onClick={handleClickLetter}>I</li>
-        <li onClick={handleClickLetter}>J</li>
-        <li onClick={handleClickLetter}>K</li>
-        <li onClick={handleClickLetter}>L</li>
-        <li onClick={handleClickLetter}>M</li>
-        <li onClick={handleClickLetter}>N</li>
-        <li onClick={handleClickLetter}>O</li>
-        <li onClick={handleClickLetter}>P</li>
-        <li onClick={handleClickLetter}>Q</li>
-        <li onClick={handleClickLetter}>R</li>
-        <li onClick={handleClickLetter}>S</li>
-        <li onClick={handleClickLetter}>T</li>
-        <li onClick={handleClickLetter}>U</li>
-        <li onClick={handleClickLetter}>V</li>
-        <li onClick={handleClickLetter}>W</li>
-        <li onClick={handleClickLetter}>X</li>
-        <li onClick={handleClickLetter}>Y</li>
-        <li onClick={handleClickLetter}>Z</li>
-      </ul> */
